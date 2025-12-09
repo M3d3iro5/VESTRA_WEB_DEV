2e4fda0e19104100c5162bd16b8dc4ea1e19676d
M3d3iro5 <lfelipeomederos@gmail.com>
2025-12-08 22:46:42 -0300

    Initial clean commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+function Home() {
+    return (
+        <div>
+            <h1>Olá meu amor, eu sei que eu pareço um idiota estudando programação
+                e que eu fico falando e e falando e falando e que as vezes vc nao entende nada
+                mas eu quero que vc saiba que eu faço tudo isso pq eu quero um futuro melhor pra gente
+                e pq eu te amo muito e quero te dar todo o conforto do mundo
+                e quero que a gente tenha uma vida incrível juntos. EU TE AMO IMENSAEMNTE MEU AMOR 
+                parece pouca coisa mas não é trivial fazer um site kkkkkk
+            </h1>
+        </div>
+    )
+}
+
+export default Home
