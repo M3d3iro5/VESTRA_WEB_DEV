6efc2ac00baa4c8df62eeff0e66409e72554aea1
M3d3iro5 <lfelipeomederos@gmail.com>
2026-01-29 13:27:23 -0300

    feat: add favicon configuration to document head

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#070707" />
+      </Head>
       <body className="bg-slate-950 text-slate-200">
         <Main />
         <NextScript />
